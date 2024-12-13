b61769eea0a0849d56f6eca7744f46f350767fc2
Gamdara <poiu12345654321@gmail.com>
2024-12-13 22:47:27 +0800

    deploy 11

--- SentradBe/config/database.php
+++ SentradBe/config/database.php
@@ -57,7 +57,11 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-            'engine' => null
+            'engine' => null,
+            'options' => [
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+                PDO::MYSQL_ATTR_SSL_CA => false,
+            ],
         ],
 
         'pgsql' => [
