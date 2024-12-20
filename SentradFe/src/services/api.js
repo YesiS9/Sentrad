ec813796b22261461b5722ef0dcf285bc721b900
YesiS9 <yesisugandi900@gmail.com>
2024-12-21 00:41:05 +0700

    ganti link2

--- SentradFe/src/services/api.js
+++ SentradFe/src/services/api.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'http://sentrad-production-2d25.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
