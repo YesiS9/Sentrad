d06d991acdaf10c3bb78d68ac205430ea4e9d4b0
yesiS9 <yesisugandi900@gmail.com>
2025-01-19 04:09:39 +0700

    fixed cors

--- SentradBe/config/cors.php
+++ SentradBe/config/cors.php
@@ -19,9 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://sentrad-git-main-yesis-projects-c3bd1885.vercel.app', 'http://localhost:5173'],
+    'allowed_origins' => [],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['^https://.*\.vercel\.app$'],
 
     'allowed_headers' => ['*'],
 
